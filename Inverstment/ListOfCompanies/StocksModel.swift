fbd3570727937607d1c5f1ac386186e47534c7f6
tasyaglk <galkina.tasya@mail.ru>
2025-06-24 19:02:51 +0300

    changed stock model

--- Inverstment/ListOfCompanies/StocksModel.swift
+++ Inverstment/ListOfCompanies/StocksModel.swift
@@ -9,20 +9,40 @@ import Foundation
 
 struct StocksModel {
     
+    //    https://financialmodelingprep.com/api/v3/stock/actives?apikey=
+    //    {
+    //        - "ticker": "SOFI",
+    //         -"changes": 0.16,
+    //         -"price": "15.36",
+    //        - "changesPercentage": "1.05263",
+    //         -"companyName": "SoFi Technologies, Inc."
+    //       }
+    
     let id: Int?
     let imageURL: String?
     let fullName: String?
     let shortName: String?
     let price: String?
+    let changesPercentage: String?
     let priceChanges: String?
     let isFavourite: Bool?
     
-    init(id: Int?, imageURL: String?, fullName: String?, shortName: String?, price: String?, priceChanges: String?, isFavourite: Bool?) {
+    init(
+        id: Int?,
+        imageURL: String?,
+        fullName: String?,
+        shortName: String?,
+        price: String?,
+        changesPercentage: String?,
+        priceChanges: String?,
+        isFavourite: Bool?
+    ) {
         self.id = id
         self.imageURL = imageURL
         self.fullName = fullName
         self.shortName = shortName
         self.price = price
+        self.changesPercentage = changesPercentage
         self.priceChanges = priceChanges
         self.isFavourite = isFavourite
     }
@@ -34,6 +54,7 @@ struct StocksModel {
         fullName: String? = nil,
         shortName: String? = nil,
         price: String? = nil,
+        changesPercentage: String? = nil,
         priceChanges: String? = nil,
         isFavourite: Bool? = nil
     ) {
@@ -42,6 +63,7 @@ struct StocksModel {
         self.fullName = fullName == nil ? stock.fullName : fullName
         self.shortName = shortName == nil ? stock.shortName : shortName
         self.price = price == nil ? stock.price : price
+        self.changesPercentage = changesPercentage == nil ? stock.changesPercentage : changesPercentage
         self.priceChanges = priceChanges == nil ? stock.priceChanges : priceChanges
         self.isFavourite = isFavourite == nil ? stock.isFavourite : isFavourite
     }
